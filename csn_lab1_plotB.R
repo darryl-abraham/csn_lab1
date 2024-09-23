149bad65ed16ab27ae8c5b7e1f5a3ac8e25586be
Ewa <emiklewska@gmail.com>
2024-09-23 15:08:59 +0200

    plotB

--- csn_lab1_plotB.R
+++ csn_lab1_plotB.R
@@ -1,32 +1,47 @@
 #install.packages("igraph")
 library(igraph)
 
-n_vals <- 2^seq(1,20,2)
+n_vals <- seq(1, 40000, 1000)
 samples <- 2
-p <- 0.3
+p <- 0.01
 
 avg_shortest_path <- vector(mode = "numeric", length = length(n_vals))
 
 for (i in seq_along(n_vals)) {
   
   n <- n_vals[i]
-  print(i)
+  print(n)
   sp_sum <- 0
-  
+
   for (j in 1:samples) {
     er_graph <- sample_gnp(n, p)
-    sp_sum <- sp_sum + average.path.length(er_graph)
+    sp <- average.path.length(er_graph)
+    sp[is.nan(sp)] <- 0
+    sp_sum <- sp_sum + sp
   }
 
   avg_shortest_path[i] <- sp_sum / samples
+  
+
+  avg_shortest_path[is.na(avg_shortest_path)] <- 0
+  y_upper_limit <- ceiling(max(avg_shortest_path))
+  x_upper_limit <- ceiling(max(n_vals))
+  
+  plot(n_vals, avg_shortest_path,
+       ylab='average shortest path',
+       xlab='num nodes',
+       xlim = c(0, x_upper_limit),
+       ylim = c(0, y_upper_limit),
+       type = "b")
 }
 
 avg_shortest_path[is.na(avg_shortest_path)] <- 0
 y_upper_limit <- ceiling(max(avg_shortest_path))
-
+x_upper_limit <- ceiling(max(n_vals))
 
 plot(n_vals, avg_shortest_path,
      ylab='average shortest path',
      xlab='num nodes',
-     xlim = c(0, 1000000),
-     ylim = c(0, y_upper_limit))
+     xlim = c(0, x_upper_limit),
+     ylim = c(0, y_upper_limit),
+     type = "b")
